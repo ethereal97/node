03a20d4f23b48ccc60671629d6e4fcfb832b95ba
Ethereal <67971655+ethereal97@users.noreply.github.com>
2020-10-23 18:29:05 +0630

    Update sessions.js

--- src/sessions.js
+++ src/sessions.js
@@ -3,28 +3,30 @@ const uuid = require("uuid").v4;
 const sessions = {};
 
 function start(user) {
-    let session_id = uuid()
+    let session_id = uuid();
     if (typeof user === 'object') {
+        user['SESSID'] = session_id;
         sessions[session_id] = user;
     }
     return session_id;
 }
 
 function use(session_id, user) {
-    sessions[session_id] = user
-    return session_id
+    sessions[session_id] = user;
+    return session_id;
 }
 
 function exists(session_id) {
-    return Boolean(sessions[session_id])
+    return Boolean(sessions[session_id]);
 }
 
 function find(session_id) {
-    return sessions[session_id]
+    return sessions[session_id];
 }
 
 function destroy(session_id) {
-    delete sessions[session_id]
+    delete sessions[session_id];
+    return uuid();
 }
 
 module.exports = {
